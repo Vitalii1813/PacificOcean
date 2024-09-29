aff14776497a50624d438cc44fc68eaf3ad7a77d
Vitalii1813 <96308455+Vitalii1813@users.noreply.github.com>
2024-09-29 23:43:31 +0300

    fix3

--- displays/PlusOcean.tsx
+++ displays/PlusOcean.tsx
@@ -74,35 +74,34 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-    fontWeight: '900',
-    color: '#7A9EA0',
+    fontWeight: "900",
+    color: "#7A9EA0",
     textAlign: 'center',
-    flex: 1,
+    marginRight: 28,
+    marginLeft: 25
   },
   settingsImg: {
-    width: 24,
-    height: 24,
-    marginRight: 10,
+    marginRight: 20,
   },
   ratingContainer: {
-    padding: 16,
+    padding: 12, // зменшено з 16
     backgroundColor: '#E2E6E9',
-    borderRadius: 20,
+    borderRadius: 16, // зменшено з 20
     width: '90%',
     alignSelf: 'center',
+    marginBottom:20
   },
   ratingTitle: {
-    fontSize: 22,
+    fontSize: 18, // зменшено з 22
     fontWeight: '600',
     textAlign: 'center',
-    marginBottom: 20,
+    marginBottom: 16, // зменшено з 20
     color: '#1C1C1C',
   },
   remindContainer: {
-    marginBottom: 20,
-    backgroundColor: '#F5F5F5',
-    borderRadius: 12,
-    padding: 10,
+    marginBottom: 16, // зменшено з 20
+    borderRadius: 10, // зменшено з 12
+    padding: 5, // зменшено з 10
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
@@ -112,48 +111,50 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    marginRight:10,
   },
   ranking: {
-    fontSize: 20,
+    fontSize: 16, // зменшено з 20
     fontWeight: 'bold',
     color: '#1C1C1C',
+    marginRight:5
   },
   trophyIcon: {
-    width: 22,
-    height: 22,
+    width: 18, // зменшено з 22
+    height: 18, // зменшено з 22
     marginLeft: 5,
+    
   },
   name: {
-    fontSize: 18,
+    fontSize: 16, // зменшено з 18
     color: '#1C1C1C',
     textAlign: 'left',
     flex: 1,
     marginLeft: 10,
   },
   weight: {
-    fontSize: 16,
+    fontSize: 14, // зменшено з 16
     fontWeight: '500',
     color: '#1C1C1C',
   },
   emptyBoxesContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 10,
+    marginTop: 8, // зменшено з 10
+    marginLeft:10
   },
   emptyBox: {
-    width: 35,
-    height: 35,
-    backgroundColor: '#90A3A3',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#5D6D7E',
+    width: 35, // зменшено з 35
+    height: 35, // зменшено з 35
+    backgroundColor: '#7A9EA0',
+    borderRadius: 6, // зменшено з 8
   },
   moonContainer: {
     alignSelf: 'center',
     marginTop: 20,
-    width: 220,
-    height: 220,
-    borderRadius: 110,
+    width: 180, // зменшено з 220
+    height: 180, // зменшено з 220
+    borderRadius: 90, // зменшено з 110
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
@@ -164,9 +165,9 @@ const styles = StyleSheet.create({
   moonImg: {
     resizeMode: 'contain',
   },
-  scrollContainer:{
-    height: 320,
-  }
+  scrollContainer: {
+    height: 270, // зменшено з 320
+  },
 });
 
 export default PlusOcean;
